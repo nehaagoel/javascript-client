39f1a5365b0d1121a742dee8031b908a08ffffa5
nehaagoel <neha.goel@successive.tech>
2020-03-12 13:27:46 +0530

    feat:children and material-ui

---
+++ src/components/Math/Math.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { operators } from '../../config/constants';
+
+export default class Math extends Component {
+  result = (first, second, operator) => {
+    let answer = first + operator + second;
+    // eslint-disable-next-line no-eval
+    answer = (operators.includes(operator)) ? eval(answer) : 'Invalid Operation';
+    return answer;
+  }
+
+  render() {
+    const {
+      first, second, operator, children,
+    } = this.props;
+    const answer = this.result(first, second, operator);
+    return (children !== undefined) ? (<p>{children(first, second, operator, answer)}</p>)
+      : (<p>{`${first} ${operator} ${second} = ${answer}`}</p>);
+  }
+}
+
+Math.propTypes = {
+  first: PropTypes.number.isRequired,
+  second: PropTypes.number.isRequired,
+  operator: PropTypes.oneOf(['+', '-', '*', '/']).isRequired,
+  children: PropTypes.func,
+};
+
+Math.defaultProps = {
+  children: undefined,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as TextFieldDemo } from './TextFieldDemo/TextFieldDemo';
 export { default as InputDemo } from './Inputs/InputDemo';
+export { default as ChildrenDemo } from './ChildrenDemo/ChildrenDemo';
