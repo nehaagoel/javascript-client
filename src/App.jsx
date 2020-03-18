267e29729363a09a99ab25dbd591e2c272d23b02
nehaagoel <neha.goel@successive.tech>
2020-03-18 17:35:51 +0530

    fix: optimise code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-// import { ThemeProvider } from '@material-ui/core/styles';
-// import { Typography } from '@material-ui/core';
-// import theme from './theme';
-// import InputDemo from './pages/Inputs/InputDemo';
 import Trainee from './pages/Trainee/Trainee';
 
 function App() {
   return (
     <>
       <Trainee />
-      {/* <ThemeProvider theme={theme}>
-        <Typography>
-          <ChildrenDemo />
-        </Typography>
-      </ThemeProvider> */}
     </>
   );
 }
