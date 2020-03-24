16f783b74f44fe1e711f8d37bf8d23147857f7e7
nehaagoel <neha.goel@successive.tech>
2020-03-24 17:40:12 +0530

    fix: modify code

--- src/layouts/components/footer/Footer.jsx
+++ src/layouts/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Box, Typography } from '@material-ui/core';
+import CopyrightIcon from '@material-ui/icons/Copyright';
+
+
+const styling = {
+  fontSize: '14px',
+};
 
 const Footer = () => (
   <>
     <Box mt={2}>
       <Typography variant="body2" color="black" align="center">
-        {' © '}
+        {/* {' © '} */}
+        <CopyrightIcon style={styling} />
       Successive Technology
       </Typography>
     </Box>
